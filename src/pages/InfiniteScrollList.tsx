b9a5d867f018ee2a75a972b9fad7b23123296496
AJoelCastro <misthorn33@gmail.com>
2025-12-02 23:04:11 -0500

    infinite scroll list

---
+++ src/pages/InfiniteScrollList.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const InfiniteScrollList = () => {
+  return (
+    <View>
+      <Text>InfiniteScrollList</Text>
+    </View>
+  )
+}
+
+export default InfiniteScrollList
