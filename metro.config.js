f63325126ff8bf52794e4d8a64254f98ac9b7879
AJoelCastro <misthorn33@gmail.com>
2025-12-04 11:15:59 -0500

    config

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,37 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const path = require('path'); // agregar
 
+const libraryRoot = path.resolve(__dirname, '../rnc-library-ntt'); // dirección de nuestra librería de componentes
 /**
  * Metro configuration
  * https://reactnative.dev/docs/metro
  *
  * @type {import('@react-native/metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+    // Observar la carpeta de la librería
+  watchFolders: [libraryRoot],
+
+  resolver: {
+    // Resolver node_modules de ambos proyectos
+    nodeModulesPaths: [
+      path.resolve(__dirname, 'node_modules'),
+      path.resolve(libraryRoot, 'node_modules'),
+    ],
+
+    // Evitar duplicación de react y react-native
+    extraNodeModules: {
+      'react': path.resolve(__dirname, 'node_modules/react'),
+      'react-native': path.resolve(__dirname, 'node_modules/react-native'),
+    },
+  },
+
+  // Configuración de watchman
+  watchman: {
+    crawl: {
+      enabled: true,
+    },
+  },
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
