135a2e6b49a51ffc02f04037bcb4c63280ed425b
AJoelCastro <misthorn33@gmail.com>
2025-12-15 19:01:01 -0500

    new dependecy

--- src/modules/transaction/screens/services/ServiceDetailScreen.tsx
+++ src/modules/transaction/screens/services/ServiceDetailScreen.tsx
@@ -1,8 +1,8 @@
-import { View, Text } from 'react-native'
+import { View, StyleSheet } from 'react-native'
 import React from 'react'
 import { RouteProp, useNavigation, useRoute } from '@react-navigation/native'
 import { HomeStackParamList } from '@/navigation/root.stack/init.tabs/home/_layout'
-import { Header } from '@arturocastro/react-native-rnc-library-ntt'
+import { Button, Header } from '@arturocastro/react-native-rnc-library-ntt'
 
 type RouteProps = RouteProp<HomeStackParamList, 'ServiceDetailHome'>
 type Props = {}
@@ -12,10 +12,26 @@ const ServiceDetailScreen = ({ }: Props) => {
     const navigate = useNavigation()
     const { id } = route.params
     return (
-        <View>
+        <View style={styles.container}>
             <Header title={`Servicio ${id}`} iconName='' onBack={() => navigate.goBack()} />
+            <View style={styles.content}>
+                <Button title='Pagar Servicio' backgroundColor='#34C759' />
+                <Button title='Cancelar' backgroundColor='#FF383C' />
+            </View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    content: {
+        flex: 1,
+        gap: 12,
+        marginHorizontal: 16,
+        marginTop: '20%'
+    },
+})
 
 export default ServiceDetailScreen
